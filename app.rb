c99abad44c6df0d121a035e11cc7ef98d20b49ea
Mark Rushakoff <mark.rushakoff@gmail.com>
2014-03-14 13:29:40 -0700

    Add sound for story rejection

--- app.rb
+++ app.rb
@@ -8,10 +8,31 @@ set :sockets, []
 
 set :haml, format: :html5
 
+CASH_SOUND = {sound: 'cash'}.to_json.freeze
+BREAK_GLASS_SOUND = {sound: 'break_glass'}.to_json.freeze
+
+EM.error_handler do |e|
+  puts 'Oops, I had an accident'
+  puts e.message
+  puts
+  puts e.backtrace
+end
+
 def send_to_all_sockets(message)
   EM.next_tick do
     settings.sockets.each do |socket|
       socket.send(message)
+    end
+  end
+end
+
+def notify_changes(changes)
+  changes.each do |change|
+    new_current_state = change.fetch('new_values', {}).fetch('current_state', nil)
+    if new_current_state == 'accepted'
+      send_to_all_sockets(CASH_SOUND.dup)
+    elsif new_current_state == 'rejected'
+      send_to_all_sockets(BREAK_GLASS_SOUND.dup)
     end
   end
 end
@@ -30,6 +51,19 @@ get '/' do
   else
     haml :index
   end
+end
+
+# try: curl -H "Content-Type: application/json" --data @fixtures/story_accepted.json http://localhost:4567/tracker
+# try: curl -H "Content-Type: application/json" --data @fixtures/story_rejected_with_comment.json http://localhost:4567/tracker
+post '/tracker' do
+  p :start
+  tracker_data = JSON.parse(request.body.read)
+
+  kind = tracker_data['kind']
+  halt(200, 'not a story update') unless kind == 'story_update_activity'
+
+  notify_changes(tracker_data.fetch('changes', []))
+  'Thank you for the changes.'
 end
 
 # try: curl -X POST --data "sound=bell" http://127.0.0.1:4567/debug
